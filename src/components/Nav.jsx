cb6c412caec8c7653b79d146d0111023a335d253
Evan Allen <evanallen13@gmail.com>
2019-10-17 16:59:58 -0700

    first

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import '../style/Nav.css'
-import { Link} from "react-router-dom";
+import { Link } from "gatsby"
 
 export default function Nav(props) {
     return (
         <div className={props.page}>
-                <Link to="/" className="navLink">Home</Link>
-                <Link to="/About" className="navLink">About</Link>
-                <Link to="/Projects" className="navLink">Projects</Link>
+                <Link to="/" style={{textDecoration : 'none'}}>
+                    <h1 className="navLink">Home</h1>
+                </Link>
+                <Link to="/About" style={{textDecoration : 'none'}}>
+                    <h1 className="navLink">About</h1>
+                </Link>
+                <Link to="/Projects" style={{textDecoration : 'none'}}>
+                    <h1 className="navLink">Projects</h1>
+                </Link>
         </div>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,20 @@
-import React from "react"
-import '../style/Index.css'
-import Homepage from './Homepage'
-import About from './About'
-import Projects from '../pages/Projects'
-import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
+import React from 'react'
+import '../style/homepage.css'
+import Nav from '../components/Nav'
+import Footer from '../components/Footer'
 
-export default (props) => 
-    <div style={{textAlign:'center'}}>
-        <title>Evan Allen</title> 
-    <Router >
-        <Switch>
-            <Route exact path="/" >
-                <Homepage background={'#2e364f'}/>
-            </Route>
-            <Route path="/About" component={About} />
-            <Route path="/Projects" component={Projects}></Route>
-        </Switch>
-    </Router>
-    </div>
+export default function Homepage() {
+    return (
+        <div className="Homepage" style={{textAlign : 'center'}}>
+            <Nav page={'Nav HomepageNav'} />
+            <h1 className='Title'>Evan A//en</h1>
+            <h4 className='ContactInfo'>(714) 264-2562</h4>
+            <h4 className='ContactInfo'>evanallen13@gmail.com</h4>
+            <h4 className='ContactInfo'>github.com/evanallen13</h4>
+            <Footer page={'Footer HomepageFooter'}></Footer>
+        </div>
+    )
+}
 
 /* 
     dark blue : #2e364f
